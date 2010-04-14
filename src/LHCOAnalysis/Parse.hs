34a23a52de559fc10a9e50b4d6b132b9d980bd44
ianwookim <ianwookim@gmail.com>
2010-04-14 02:35:58 +0000

    20100413

--- src/LHCOAnalysis/Parse.hs
+++ src/LHCOAnalysis/Parse.hs
@@ -15,34 +15,14 @@ import Data.ByteString.Lex.Lazy.Double
 import Data.ByteString.Internal  
 import qualified Data.ByteString.Lazy.Char8 as B
 
---import Text.ParserCombinators.Parsec
---import Text.Parsec.ByteString.Lazy 
---import qualified Text.ParserCombinators.Parsec.Token as P
---import Text.ParserCombinators.Parsec.Language (haskellDef)
-
-import Text.Parsec
-import Text.Parsec.ByteString.Lazy
---import Text.Parsec.ByteString
-import qualified Text.Parsec.Token as P
-import Text.Parsec.Language (haskellDef)
 
 data Parsed = Comment B.ByteString | Zero Int | Nonzero (Int,EachObj) | Strange
               deriving (Show)
 
 
-{--
-run :: Parser a -> B.ByteString -> Either String a 
-run p input = 
-    case (parse p "" input) of 
-      Left err -> Left $"parse error at " ++ show err ++ "\n" 
-      Right x  -> Right x
---}
-
 parsestr str1 = 
     let strlines = B.lines str1 
---       myparser = run lineparser 
         parsed = map classify_line strlines
---        filtered = map  $ filter (isRight) parsed
         commentfiltered = filter (not.isComment) parsed
         strangefiltered = filter (not.isStrange) commentfiltered
         splitted  = SP.splitWhen isZero strangefiltered 
@@ -151,128 +131,3 @@ addEachObjtoClassified phyclass (idx,(EO bjet@(ObjBJet _ _)))
 addEachObjtoClassified phyclass (idx,(EO met'@(ObjMET _)))  
   = phyclass { met         = met' }
 
-{--
-
-lineparser = do  (try commentline >>= return.Comment )
-                 <|> (try zeroline >>= return.Zero   )
-                 <|> (try nonzeroline >>= return.Nonzero )
-                 <|> return Strange
- 
-
-
-
-
-
-lexer   = P.makeTokenParser haskellDef    
-            
-integer = P.integer lexer
-float   = P.float lexer
-                         
-
-
---parseLHCO :: String -> Either ParseError [(Int,PhyEventClassified)]  
---parseLHCO inputfilename = parseFromFile lhcoFile inputfilename
-
-{--
-lhcoFile :: GenParser Char8 st [(Int,PhyEventClassified)]
-lhcoFile = do many (try commentline) -- <?> "nocommentline"
-              events <- many eventblock
-              eof 
-              return events
-
-eventblock :: GenParser Char8 st (Int,PhyEventClassified)
-eventblock = do eventnumber <- zeroline <?> "nozeroline"
-                eventbody <- nonzerolines
-                return (eventnumber,eventbody)
---}
-                
-zeroline :: GenParser B.ByteString st Int
-zeroline = do empty
-              char '0'
-              empty
-              eventnumber <- myint
---              let eventnumber = trace (show eventnumber') eventnumber'
---              let eventnumber = readsPrec eventnumberstr
-              empty 
-              myint
-              empty
-              return eventnumber
-{--              
-nonzerolines :: GenParser Char8 st PhyEventClassified               
-nonzerolines = do unclassified <- many (try nonzeroline)
-                  let classified = foldl addEachObjtoClassified zeroevent unclassified
-                      sortedclassified = sortPhyEventC classified 
-                  return classified
---}
-                
-nonzeroline :: GenParser B.ByteString st (Int,EachObj)
-nonzeroline = do empty
-                 objid <- myintnonzero
---                 let objid = trace (show objid') objid'
-                 empty
-                 typ  <- myint
-                 empty 
-                 eta <- myroughfloat
-                 empty
-                 phi <- myroughfloat
-                 empty
-                 pt <- myroughfloat
-                 empty
-                 jmas <- myroughfloat
-                 empty 
-                 ntrk <- myroughfloat
-                 empty
-                 btag <- myroughfloat
-                 empty
-                 hadem <- myroughfloat
-                 empty
-                 dum1 <- myroughfloat
-                 empty
-                 dum2 <- myroughfloat
-                 empty
-                 many (noneOf "\n\r") 
-                 return $ (objid,  mkEachObj typ eta phi pt jmas ntrk  
-                                             btag hadem dum1 dum2 )
-                 
-
-
-
-{--                 
-integer' :: GenParser Char st Int                  
-integer' = do result <- integer
-              return $ fromInteger result
---}           
-                 
-myintnonzero :: GenParser B.ByteString st Int
-myintnonzero = do i <- oneOf "123456789"
-                  rest <- many (oneOf "0123456789")
-                  return $ read (i:rest)
-
-myint :: GenParser B.ByteString st Int
-myint = do rest <- many (oneOf "0123456789")
-           return $ read rest
-
-
-myroughfloat :: GenParser B.ByteString st Double
-myroughfloat = do result <- many (oneOf "+-0123456789.")
-                  return $ read result
-                  
-commentline :: GenParser B.ByteString st B.ByteString
-commentline = do empty 
-                 char '#' 
-                 many (noneOf "\n\r") 
-                 return $ B.pack "COMMENT" 
-
-{--
-eol :: GenParser Char8 st String
-eol = try (string "\n\r")
-      <|> (string "\r\n")
-      <|> string "\n"
-      <|> string "\r"
---} 
-
-empty = many (oneOf emptyletters)
-
-emptyletters = [' ']
---parseLHCO 
---}
